9af1398c843eba175913067d5ae0ebbfee51ad3f
Adam3192 <jeremyadixon@hotmail.com>
2022-10-01 12:38:34 -0500

    react router setup and single project query

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
-import Clients from './components/Clients'
-import Projects from './components/Projects'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-import AddClientModal from './components/AddClientModal'
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -10,17 +11,17 @@ const cache = new InMemoryCache({
       fields: {
         clients: {
           merge(existing, incoming) {
-            return incoming;
-          }
+            return incoming
+          },
         },
         projects: {
           merge(existing, incoming) {
-            return incoming;
-          }
+            return incoming
+          },
         },
-      }
-    }
-  }
+      },
+    },
+  },
 })
 
 const client = new ApolloClient({
@@ -32,12 +33,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   )
